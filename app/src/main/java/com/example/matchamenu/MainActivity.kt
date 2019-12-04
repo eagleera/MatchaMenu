40d1e10f7bf3ae049b645d0185ed7bd2d3cb2aca
Eaglewas <daguilera3220@gmail.com>
2019-12-03 18:00:08 -0700

    se agrega splashScreen

--- app/src/main/java/com/example/matchamenu/MainActivity.kt
+++ app/src/main/java/com/example/matchamenu/MainActivity.kt
@@ -11,10 +11,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
