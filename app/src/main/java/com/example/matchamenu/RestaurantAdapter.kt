9a619118378418efebf14f63f26491671d9a9e6c
Eaglewas <daguilera3220@gmail.com>
2019-12-04 16:28:40 -0700

    se agrega clickListener

--- app/src/main/java/com/example/matchamenu/RestaurantAdapter.kt
+++ app/src/main/java/com/example/matchamenu/RestaurantAdapter.kt
@@ -1,11 +1,14 @@
 package com.example.matchamenu
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
+import android.widget.Toast
+
 
 class RestaurantAdapter(val mCtx: Context, val layoutResId: Int, val restaurantList: List<Restaurant>)
     : ArrayAdapter<Restaurant>(mCtx, layoutResId, restaurantList) {
@@ -17,6 +20,11 @@ class RestaurantAdapter(val mCtx: Context, val layoutResId: Int, val restaurantL
         val textViewName = view.findViewById<TextView>(R.id.textViewRestaurant)
         val restaurant = restaurantList[position]
         textViewName.text = restaurant.name
+        textViewName.setOnClickListener{
+            val intent = Intent(this@FirstActivity, SecondActivity::class.java)
+
+            Toast.makeText(mCtx, restaurant.id, Toast.LENGTH_LONG).show()
+        }
         return view;
     }
 }
