b8767913006071ccb44abde17ef30bf76c7fab32
Christian Calderon <chris.calderon.ortega@gmail.com>
2019-12-04 17:48:48 -0700

    Se agregan Menu nuevo

--- app/src/main/java/com/example/matchamenu/Menu.kt
+++ app/src/main/java/com/example/matchamenu/Menu.kt
@@ -1,12 +1,22 @@
 package com.example.matchamenu
 
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class Menu : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+
+        btnFav.setOnClickListener {
+            btnFav.backgroundTintList =(ColorStateList.valueOf(Color.parseColor("#FFFFFF")))
+        }
     }
 }
+
+
